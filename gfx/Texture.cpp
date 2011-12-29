6afd6fa33269d47133889f8f85013272b6578d08
David Koerner <david-k@moving-picture.com>
2011-12-29 14:43:48 +0000

    forgot to set magfilter in texture2d

--- gfx/Texture.cpp
+++ gfx/Texture.cpp
@@ -172,7 +172,7 @@ namespace base
 		//glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
 		// when texture area is large, bilinear filter the original
 		//glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-		//glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 		//glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR );
 
 		// the texture wraps over at the edges (repeat)
