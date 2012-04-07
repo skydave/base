c68df4e816a221edbc8241d23ece4fa46be68974
David Koerner <dk402538@googlemail.com>
2012-04-07 19:45:37 +0200

    added initial iteration of bson stuff - still a bit flakey and short of features...

---
+++ util/bson.cpp
@@ -0,0 +1,300 @@
+
+#include "BSON.h"
+
+#include <vector>
+
+namespace base
+{
+	namespace bson
+	{
+		std::ostream & serialiseInt(std::ostream & out, const int n )
+		{
+			int t = n;
+			out.write( (const char *)&t, sizeof(int) );
+			return out;
+		}
+		std::istream & deserialiseInt(std::istream & in, int &n )
+		{
+			in.read( ( char *)&n, sizeof(int) );
+			return in;
+		}
+
+
+		std::ostream & serialiseString(std::ostream & out, const std::string &str )
+		{
+			serialiseInt( out, (int)str.size() );
+			out.write( str.c_str(), str.size() );
+			return out;
+		}
+
+		std::istream & deserialiseString(std::istream & in, std::string &str )
+		{
+			int size = 0;
+			deserialiseInt( in, size );
+			std::vector<char> tmp;
+			tmp.resize( size );
+			in.read( (char *)(&tmp[0]), size );
+			str = std::string( tmp.begin(), tmp.end() );
+			return in;
+		}
+
+
+		// ===============================================
+		// Key
+		// ===============================================
+		Key::Key()
+		{
+		}
+
+		Key::Key( const std::string &key ) : m_isString(true), m_stringKey(key)
+		{
+		}
+
+		Key::Key( const int key )
+		{
+		}
+
+		int Key::hash() const
+		{
+			std::hash<std::string> test;
+			if( m_isString )
+				return (int) test(m_stringKey);
+			return m_intKey;
+		}
+
+		bool operator<( const Key &lhs, const Key &rhs )
+		{
+			return lhs.hash() < rhs.hash();
+		}
+
+		std::ostream & operator<<(std::ostream & out, const Key &key )
+		{
+			out.write( (char *)(&key.m_isString), sizeof(bool) );
+			if(key.m_isString)
+				serialiseString(out, key.m_stringKey);
+			else
+				out.write( (char *)(&key.m_intKey), sizeof(int) );
+
+			return out;
+		}
+		std::istream & operator>>(std::istream & in, Key &key )
+		{
+			in.read( (char *)(&key.m_isString), sizeof(bool) );
+			if(key.m_isString)
+				deserialiseString(in, key.m_stringKey);
+			else
+				in.read( (char *)(&key.m_intKey), sizeof(int) );
+
+			return in;
+		}
+
+		// ===============================================
+		// Item
+		// ===============================================
+		Item::Item() : m_data(0), m_size(0), m_child(BSONPtr())
+		{
+
+		}
+
+		std::ostream & operator<<(std::ostream & out, const Item &item )
+		{
+			bool hasChild = false;
+			if(item.m_child)
+				hasChild = true;
+			out.write( (char *)(&hasChild), sizeof(bool) );
+			if( !hasChild )
+			{
+				out.write( (char*)(&item.m_size), sizeof(int));
+				out.write( item.m_data, item.m_size );
+			}else
+				out << *(item.m_child);
+
+			return out;
+		}
+
+		std::istream & operator>>(std::istream & in, Item &item )
+		{
+			bool hasChild = false;
+			in.read( (char *)(&hasChild), sizeof(bool) );
+			if( !hasChild )
+			{
+				in.read( (char *)&item.m_size, sizeof(int) );
+				item.m_data = (char *)malloc(sizeof(char)*item.m_size);
+				in.read( item.m_data, item.m_size );
+			}else
+			{
+				BSON *child = new BSON();
+				in >> *child;
+				item.m_child = BSONPtr(child);
+			}
+
+			return in;
+		}
+
+
+		// ===============================================
+		// ItemHelper
+		// ===============================================
+		ItemHelper::ItemHelper() : m_bson(BSONPtr())
+		{
+
+		}
+
+		ItemHelper::ItemHelper( BSONPtr bson, const Key &key ) : m_bson(bson), m_key(key)
+		{
+			m_item = m_bson->get(key);
+		}
+
+		std::string ItemHelper::asString()
+		{
+			return std::string( m_item->m_data, m_item->m_size );
+		}
+
+		// will only work if wrapped item is a bson object
+		ItemHelper ItemHelper::operator[]( const std::string &key )
+		{
+			if( m_item->m_child )
+				return ItemHelper( m_item->m_child, key );
+			// TODO:we probably should assert
+			return ItemHelper();
+		}
+
+
+		// ===============================================
+		// BSON
+		// ===============================================
+		BSON::BSON() : m_nextIndex(0)
+		{
+		}
+
+		ItemPtr BSON::get( const Key &key )
+		{
+			ItemMap::iterator it = m_items.find(key);
+
+			if( it != m_items.end() )
+				return it->second;
+
+			return ItemPtr();
+		}
+
+		void BSON::set( const Key &key, ItemPtr item )
+		{
+			m_items[key] = item;
+		}
+
+		// uses a local counter
+		void BSON::append( ItemPtr item )
+		{
+			set(m_nextIndex++, item);
+		}
+
+		int BSON::numItems() const
+		{
+			return (int)m_items.size();
+		}
+
+		Helper create()
+		{
+			return Helper(BSONPtr( new BSON() ));
+		}
+
+
+		std::ostream & operator<<(std::ostream & out, const BSON &bson )
+		{
+			int numItems = (int)bson.m_items.size();
+			out.write( (char*)(&numItems), sizeof(int));
+
+			for( BSON::ItemMap::const_iterator it = bson.m_items.begin(); it != bson.m_items.end();++it )
+			{
+				out << it->first;
+				out << *(it->second);
+			}
+
+			return out;
+		}
+
+		std::istream & operator>>(std::istream & in, BSON &bson )
+		{
+			int numItems = 0;
+			in.read( (char *)&numItems, sizeof(int) );
+
+			for( int i=0;i<numItems;++i )
+			{
+				Key key;
+				Item *item = new Item();
+				in >> key;
+				in >> *item;
+				bson.set( key, ItemPtr(item) );
+			}
+
+			return in;
+		}
+
+
+		// ===============================================
+		// Helper
+		// ===============================================
+
+		ItemHelper Helper::operator[]( const std::string &key )
+		{
+			return ItemHelper( m_bson, key );
+		}
+
+
+		Helper::Helper( BSONPtr bson ) : m_bson(bson)
+		{
+		}
+
+		Helper::operator BSONPtr()
+		{
+			return m_bson;
+		}
+
+
+		// ===============================================
+		// Packet
+		// ===============================================
+
+		Packet::Packet() : m_data(0), m_size(0)
+		{
+
+		}
+
+		Packet::Packet( const std::string &data )
+		{
+			m_size = (int)data.size();
+			m_data = (char *)malloc(sizeof(char)*m_size);
+			memcpy( m_data, data.c_str(), m_size );
+		}
+
+		Packet::~Packet()
+		{
+			if( m_data )
+				free(m_data);
+			m_size=0;
+		}
+
+		PacketPtr pack( Helper &helper )
+		{
+			std::ostringstream out(std::stringstream::out | std::stringstream::binary);
+
+			out << *(helper.m_bson);
+			PacketPtr p = PacketPtr(new Packet(out.str()));
+			return p;
+		}
+
+		Helper unpack( PacketPtr &packet )
+		{
+			base::bson::OneShotReadBuf osrb(packet->m_data, packet->m_size);
+			std::istream in( &osrb );
+			//std::istringstream in( packet->m_data, std::stringstream::in | std::stringstream::binary );
+
+			BSON *b = new BSON();
+
+			in >> *b;
+
+			return Helper(BSONPtr( b ));
+
+		}
+	}
+}
---
+++ util/bson.h
@@ -0,0 +1,265 @@
+#pragma once
+
+#include <util/shared_ptr.h>
+
+#include <string>
+#include <map>
+#include <sstream>
+
+namespace base
+{
+	namespace bson
+	{
+		BASE_DECL_SMARTPTR_STRUCT(BSON);
+		BASE_DECL_SMARTPTR_STRUCT(Packet);
+
+		// http://stackoverflow.com/questions/1448467/initializing-a-c-stdistringstream-from-an-in-memory-buffer/1449527#1449527
+		struct OneShotReadBuf : public std::streambuf
+		{
+			OneShotReadBuf(char* s, std::size_t n)
+			{
+				setg(s, s, s + n);
+			}
+		};
+
+
+		std::ostream & serialiseInt(std::ostream & out, const int n );
+		std::istream & deserialiseInt(std::istream & in, int &n );
+
+		std::ostream & serialiseString(std::ostream & out, const std::string &str );
+		std::istream & deserialiseString(std::istream & in, std::string &str );
+
+		// ===============================================
+		// Key
+		// ===============================================
+
+		struct Key
+		{
+			Key();
+			Key( const std::string &key );
+			Key( const int key );
+
+			int               hash() const;
+
+			bool                m_isString; // if false, key is int
+			std::string        m_stringKey;
+			int                   m_intKey;
+
+
+
+
+		};
+
+		bool operator<( const Key &lhs, const Key &rhs );
+		std::ostream & operator<<(std::ostream & out, const Key &key );
+		std::istream & operator>>(std::istream & in, Key &key );
+
+		// ===============================================
+		// Item
+		// ===============================================
+
+		BASE_DECL_SMARTPTR_STRUCT(Item);
+		struct Item
+		{
+			Item();
+
+			template<typename T>
+			void set( const T &value );
+
+			char          *m_data;
+			int            m_size;
+			BSONPtr       m_child;
+		};
+
+		template<>
+		inline void Item::set<std::string>(const std::string &other)
+		{
+			m_size = (int)other.size();
+			m_data = (char *)realloc( m_data, m_size );
+			memcpy(m_data, &other[0], m_size);
+			if(m_child)
+				m_child = BSONPtr();
+		}
+
+		template<>
+		inline void Item::set<BSONPtr>(const BSONPtr &other)
+		{
+			m_size = 0;
+			if( m_data )
+			{
+				free(m_data);
+				m_data = 0;
+			}
+			m_child = other;
+		}
+
+		std::ostream & operator<<(std::ostream & out, const Item & );
+		std::istream & operator>>(std::istream & in, Item &item );
+	
+		// ===============================================
+		// ItemHelper
+		// ===============================================
+
+		struct ItemHelper
+		{
+			ItemHelper();
+			ItemHelper( BSONPtr bson, const Key &key );
+
+			ItemHelper      operator[]( const std::string &key ); // will only work if wrapped item is a bson object
+
+			// must support pods, bson objects
+			template<typename T>
+			ItemHelper & operator = (const T &other);
+
+			template<typename T>
+			operator T ();
+
+			std::string asString();
+
+
+			BSONPtr                           m_bson;
+			Key                                m_key;
+			ItemPtr                           m_item; // the item which is wrapped by the helper - invalid pointer means: item not yet created
+		};
+
+		// ItemHelper assignment operator
+		template<typename T>
+		ItemHelper & ItemHelper::operator = (const T &other)
+		{
+			if(!m_item)
+			{
+				m_item = ItemPtr(new Item());
+				// no items means, it hasnt been added to the BSON yet
+				m_bson->set( m_key, m_item );
+			}
+			m_item->set<T>(other);
+
+			return *this;
+		}
+
+		template <typename T>
+		ItemHelper::operator T()
+		{
+			T data;
+			memcpy( &data, m_item->m_data, sizeof(T));
+			return data;
+		}
+
+
+
+		// for some reason I didnt got template specializatino to work for strings :(
+		//inline operator std::string()
+		//{
+		//	std::string data( (char *) m_item->m_data, m_item->m_size );
+		//	return data;
+		//}
+
+
+
+
+
+		// ===============================================
+		// Helper
+		// ===============================================
+
+		struct Helper
+		{
+			Helper(BSONPtr bson);
+
+
+			ItemHelper      operator[]( const std::string &key ); // select an item within the bson object
+
+			template<typename T>
+			Helper                  &operator+=( const T value ); // append item
+
+			operator BSONPtr ();                                  // ptr conversion operator
+
+
+			BSONPtr m_bson;
+		};
+
+
+		// for some reason I didnt got template specializatino to work for strings :(
+		template <>
+		inline ItemHelper::operator Helper()
+		{
+			return Helper(m_item->m_child);
+		}
+
+
+		// ===============================================
+		// BSON
+		// ===============================================
+		struct BSON
+		{
+			typedef std::map<Key, ItemPtr> ItemMap;
+
+			BSON();
+
+			ItemPtr                             get( const Key &key );
+			void                  set( const Key &key, ItemPtr item );
+			void                               append( ItemPtr item ); // uses a local counter
+			int                                      numItems() const;
+
+			ItemMap                                           m_items;
+			int                                           m_nextIndex;
+
+		};
+		std::ostream & operator<<(std::ostream & out, const BSON & );
+		std::istream & operator>>(std::istream & in, BSON & );
+
+		Helper create();
+
+
+
+	
+		template<typename T>
+		Helper & Helper::operator+=( const T value )
+		{
+			ItemPtr item = ItemPtr(new Item());
+			item->set<T>(value);
+			m_bson->append( item );
+			return *this;
+		}
+
+		// ===============================================
+		// Item
+		// ===============================================
+		template<typename T>
+		void Item::set( const T &item )
+		{
+			m_size = sizeof(T);
+			m_data = (char *)realloc( m_data, m_size );
+			memcpy(m_data, &item, m_size);
+			if(m_child)
+				m_child = BSONPtr();
+		}
+		template<>
+		inline void Item::set( const Helper &child )
+		{
+			if( m_data )
+			{
+				m_size = 0;
+				free(m_data);
+			}
+			m_child = child.m_bson;
+		} 
+
+
+		// ===============================================
+		// Packet
+		// ===============================================
+		struct Packet
+		{
+			Packet( const std::string &data );
+			Packet();
+			~Packet();
+			char          *m_data;
+			int            m_size;
+		};
+
+		PacketPtr pack( Helper &helper );
+		Helper unpack( PacketPtr &packet );
+		
+	}
+}
