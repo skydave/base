68b76aab09e9358a717702004ee5b53c12eb5275
David Koerner <dk402538@googlemail.com>
2012-05-16 23:20:24 +0200

    extended types.h

--- util/types.h
+++ util/types.h
@@ -5,8 +5,26 @@
 typedef signed __int64        sint64;
 typedef unsigned __int64      uint64;
 typedef unsigned __int32      uint32;
+typedef float                 real32;
+typedef double                real64;
+
+typedef signed   char          sbyte;
+typedef unsigned char          ubyte;
+typedef signed   short int     sword;
+typedef unsigned short int     uword;
+typedef signed   long  int    sdword;
+typedef unsigned long  int    udword;
 #else
 typedef unsigned long long    uint64;
 typedef signed long long      sint64;
 typedef unsigned int          uint32;
+typedef float                 real32;
+typedef double                real64;
+
+typedef signed   char          sbyte;
+typedef unsigned char          ubyte;
+typedef signed   short int     sword;
+typedef unsigned short int     uword;
+typedef signed   long  int    sdword;
+typedef unsigned long  int    udword;
 #endif
